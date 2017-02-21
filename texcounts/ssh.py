3aec5e99a1591bf65e67f8647f31b365b678d65c
scienceopen <scienceopen@users.noreply.github.com>
2017-02-21 11:30:02 -0500

    pythonize

--- texcounts/ssh.py
+++ texcounts/ssh.py
@@ -1,16 +1,16 @@
 from paramiko import SSHClient
-import getpass
+from getpass import getpass
 
 def uploadSFTP(username,serverAddress,serverDir,texfn):
     imgfn = texfn.with_suffix('.png')
 
-    print('Uploading {} to {} {}'.format(imgfn, serverAddress ,serverDir))
+    print(f'Uploading {imgfn} to {serverAddress} {serverDir}')
 
     ssh = SSHClient()
     #ssh.set_missing_host_key_policy(AutoAddPolicy())
     #ssh.load_host_keys(expanduser(join("~", ".ssh", "known_hosts")))
     ssh.load_system_host_keys()
-    ssh.connect(serverAddress, username=username,password=getpass.getpass(prompt='password: '))
+    ssh.connect(serverAddress, username=username, password=getpass())
     sftp = ssh.open_sftp()
     sftp.put(imgfn, serverDir+imgfn.name,confirm=True) #note that destination filename MUST be included!
     sftp.close()
