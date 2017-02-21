3da912f3bd27f397f9a308812605ba5ae2230f7c
scienceopen <scienceopen@users.noreply.github.com>
2017-02-21 11:52:51 -0500

    pythonic

--- setup.py
+++ setup.py
@@ -1,21 +1,17 @@
 #!/usr/bin/env python
 from setuptools import setup #enables develop
-try:
-    import conda.cli
-    conda.cli.main('install','--file','requirements.txt')
-except Exception as e:
-    print(e)
-    import pip
-    pip.main(['install','-r','requirements.txt'])
+
+req = ['nose','paramiko','numpy','matplotlib']
 
 #%% install
 
 setup(name='texcounts',
+      packages=['texcounts'],
 	  description='Count pages, figures, and tables for publication page count.',
 	  author='Michael Hirsch, Ph.D.',
 	  url='https://github.com/scienceopen/texcounts',
 	  classifiers=[
 	        'Programming Language :: Python :: 3.6',
 	        ],
-      packages=['texcounts'],
+	  install_requires=req,
 	  )
