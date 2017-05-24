dd9a3627880eae5fa9cb760d7ee088d9e755e905
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2017-05-24 14:42:31 -0400

    template

--- setup.py
+++ setup.py
@@ -1,17 +1,22 @@
 #!/usr/bin/env python
-from setuptools import setup #enables develop
-
 req = ['nose','paramiko','numpy','matplotlib']
 
+import pip
+try:
+    import conda.cli
+    conda.cli.main('install',*req)
+except Exception as e:    
+    pip.main(['install'] + req)
+
 #%% install
+from setuptools import setup #enables develop
 
 setup(name='texcounts',
       packages=['texcounts'],
 	  description='Count pages, figures, and tables for publication page count.',
 	  author='Michael Hirsch, Ph.D.',
-	  url='https://github.com/scienceopen/texcounts',
+	  url='https://github.com/scivision/texcounts',
 	  classifiers=[
 	        'Programming Language :: Python :: 3.6',
 	        ],
-	  install_requires=req,
 	  )
